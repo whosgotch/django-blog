a635ce65c219db8b5f0f7a4d95949ea3c2d80783
whosgotch <komovaar@gmail.com>
2023-02-28 18:17:23 +0200

    Created 'Blog' app and tested 'models.py'

---
+++ blogger/blogger/tests/factories.py
@@ -0,0 +1,26 @@
+import factory
+from django.contrib.auth.models import User
+
+from blogger.blog.models import Post
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    password = "test"
+    username = "test"
+    is_superuser = True
+    is_staff = True
+
+
+class PostFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Post
+
+    title = "x"
+    subtitle = "x"
+    slug = "x"
+    author = factory.SubFactory(UserFactory)
+    content = "x"
+    status = "published"
